5b0ef14d35e3059866e7c9ab18edab649666ebc7
Alex Ten <orionapplepie@gmail.com>
2018-11-23 17:18:59 +1000

    Добавлены модели инвентаря и дистрибутива ПО.

--- common/models.py
+++ common/models.py
@@ -1,3 +1,39 @@
+# coding: utf-8
 from django.db import models
 
-# Create your models here.
+class Inventory(models.Model):
+    """Модель инвентаря - компьютер или сервер."""
+    INV_TYPES = (
+        (0, 'Сервер'),
+        (1, 'Простой АРМ'),
+        (2, 'Ноутбук')
+    )
+    title = models.CharField(max_length=50)
+
+    inventory_type = models.IntegerField(
+        max_length = 24,
+        choices=INV_TYPES
+    )
+    
+    inventory_number = models.CharField(max_length=16)
+    
+    def __str__(self):
+        return "%s " % (self.title)
+
+
+class Software(models.Model):
+    """Модель для ПО."""
+    title = models.CharField(
+        blank=False,
+        null=False,
+        max_length=50
+    )
+    
+    serial = models.CharField(max_length=50)
+
+    inventory = models.ForeignKey(
+        Inventory, on_delete=models.CASCADE
+    )
+    
+    def __str__(self):
+        return "%s " % (self.title)
